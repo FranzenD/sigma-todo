ff8a54ff7f80898b43a59354f46280b26713bc8d
Daniel Franzén <Daniel@Daniels-MBP-2.lan>
2018-10-23 00:12:20 +0200

    Added TodoList components

---
+++ src/components/TodoList.vue
@@ -0,0 +1,76 @@
+<template>
+<div class="todo-list">
+    <input type="text" class="todoInput" v-model="newTodo" @keyup.enter="addTodo" placeholder="Add todo..."/>
+
+    <div v-for="todo in todoList" :key="todo.id">
+        <todo-list-item :todo-item="todo"
+                        @delete="deleteTodo"
+                        @complete="completeTodo"></todo-list-item>
+    </div>
+    <div>Numer of todos: {{numberOfTodos}}</div>
+</div>
+</template>
+
+<script>
+import TodoListItem from "./TodoListItem.vue";
+
+let todoUid = 0;
+const Status = {
+  done: 0,
+  active: 1
+};
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoListItem
+  },
+  data() {
+    return {
+      todoList: [],
+      newTodo: ""
+    };
+  },
+  props: {},
+  methods: {
+    addTodo() {
+      this.todoList.push({
+        id: todoUid++,
+        todoText: this.newTodo,
+        status: Status.active
+      });
+      this.newTodo = "";
+    },
+    deleteTodo(todoId) {
+      let index = this.todoList.findIndex(item => item.id === todoId);
+      if (index > -1) {
+        this.todoList.splice(index, 1);
+      }
+    },
+    completeTodo(todoId) {
+      this.todoList.find(item => item.id === todoId).status = Status.done;
+    }
+  },
+  computed:{
+      numberOfTodos(){
+          return this.todoList.length;
+      }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-list {
+  width: 60%;
+  margin: 0 auto;
+}
+.todoInput {
+  margin-bottom: 20px;
+  width: 100%;
+  padding: 10px;
+  font-size: 24px;
+  &:focus{
+      outline: none;
+  }
+}
+</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="todo-item" :class="{'todo-item--done': todoItem.status === status.done}">
+        <div class="todo-item__text">{{todoItem.todoText}}</div>
+        <div class="todo-item__action">
+            <span v-show="todoItem.status === status.active" 
+                @click="completeTodo">Klarmarkera</span>
+            <span @click="deleteTodo">Ta bort</span></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TodoListItem",
+  data() {
+    return {
+      isEditing: false,
+      status: {
+        done: 0,
+        active: 1
+      }
+    };
+  },
+  props: {
+    todoItem: Object
+  },
+  methods: {
+    deleteTodo() {
+      this.$emit("delete", this.todoItem.id);
+    },
+    completeTodo() {
+      this.$emit("complete", this.todoItem.id);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-item {
+  border: solid 1px black;
+  margin-bottom: 10px;
+  padding: 5px;
+  display: flex;
+  justify-content: space-between;
+  &--done {
+    color: white;
+    border-color: green;
+    background: rgb(74, 167, 67)
+  }
+  &__action {
+    cursor: pointer;
+    display: inline-flex;
+    & span:first-child {
+      margin-right: 10px;
+    }
+    & span:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
